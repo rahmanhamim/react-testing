c90d71bf7d2da5cd23191933770a07e078402104
rahmanhamim <rahmanhamiminfo@gmail.com>
2023-03-14 16:43:43 +0600

    user-event click

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import Application from "./components/Application/Application";
-import MapComponent from "./components/Map/MapComponent";
+import Counter from "./components/Counter/Counter";
 import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <div className="App">
-      {/* <Application /> */}
-      {/* <Skills skills={["HTML", "CSS", "JS"]} /> */}
-      <MapComponent />
+      <Application />
+      <Skills skills={["HTML", "CSS", "JS"]} />
+      {/* <MapComponent /> */}
+      <Counter />
     </div>
   );
 }
